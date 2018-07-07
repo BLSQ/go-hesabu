461ba231d362ca85aad3896808ee503a46b177a2
mestachs <mestachs>
2018-07-07 09:19:22 +0200

    Add rest service mode

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ go get -u github.com/Knetic/govaluate
 go get -u github.com/derekparker/delve/cmd/dlv
 ```
 
+# From the command line
 Taking equations
 
 ```json
@@ -49,5 +50,38 @@ go run hesabu.go ./small.json
   "sum2": 10.54,
   "sum3": 64
 }
+
+```
+
+# As a rest service
+
+start the server
+
+```
+go run hesabu.go s
+```
+
+and post problem and get solutions
+
+```
+time curl -H "Content-Type: application/json"  --data @small.json http://127.0.0.1:8080
+{
+  "a": 10,
+  "avg1": 2.5,
+  "b": 20,
+  "c": 210,
+  "max1": 100,
+  "max2": 100,
+  "max3": 100,
+  "min1": 10,
+  "min2": 10,
+  "min3": 10,
+  "sum1": 10.54,
+  "sum2": 10.54,
+  "sum3": 64
+}
+real	0m0.009s
+user	0m0.004s
+sys	0m0.000s
 
 ```
--- hesabu.go
+++ hesabu.go
@@ -1,30 +1,55 @@
 package main
 
 import (
+	"./hesabu"
 	"encoding/json"
 	"fmt"
-
-	"./hesabu"
-
+	"github.com/NYTimes/gziphandler"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 )
 
 func init() {
-	//log.SetOutput(ioutil.Discard)
+	log.SetOutput(ioutil.Discard)
+}
+func handler(w http.ResponseWriter, request *http.Request) {
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		panic(err)
+	}
+	//log.Println(string(body))
+	var reqRawEquations map[string]interface{}
+	err = json.Unmarshal(body, &reqRawEquations)
+	if err != nil {
+		log.Printf("equations not loaded %v ", err)
+	}
+	rawEquations := make(map[string]string, len(reqRawEquations))
+	for k, v := range reqRawEquations {
+		rawEquations[k] = fmt.Sprintf("%v", v)
+	}
+	parsedEquations := hesabu.Parse(rawEquations, hesabu.Functions())
+
+	solutions := parsedEquations.Solve()
+	b, _ := json.MarshalIndent(solutions, "", "  ")
+	s := string(b)
+	fmt.Fprintf(w, s)
 }
 
 func main() {
 
-	rawEquations := getEquations(os.Args[1])
-
-	functions := hesabu.Functions()
-
-	parsedEquations := hesabu.Parse(rawEquations, functions)
-
-	solutions := parsedEquations.Solve()
-	logSolution(solutions)
+	if os.Args[1] == "s" {
+		withoutGz := http.HandlerFunc(handler)
+		withGz := gziphandler.GzipHandler(withoutGz)
+		http.Handle("/", withGz)
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	} else {
+		rawEquations := getEquations(os.Args[1])
+		parsedEquations := hesabu.Parse(rawEquations, hesabu.Functions())
+		solutions := parsedEquations.Solve()
+		logSolution(solutions)
+	}
 }
 
 func logSolution(solutions map[string]interface{}) {
