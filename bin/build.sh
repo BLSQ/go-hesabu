2f3c156345107d9f8f3c62e6713876244f950750
pjaspers <pjaspers@bluesquarehub.com>
2019-01-07 16:10:28 +0100

    Let docker script use script/build

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp $1 golang:1.11 go build -v
+docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp $1 golang:1.11 script/build
