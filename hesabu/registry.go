e71f12e74792a3737e4ee3552757bdf6a13413e4
pjaspers <pjaspers@pjmini.local>
2020-06-25 10:08:29 +0200

    Make typeCheck more general

--- hesabu/registry.go
+++ hesabu/registry.go
@@ -18,53 +18,55 @@ func (e *customFunctionError) Error() string {
 	return fmt.Sprintf("Error for %s-function, %s", e.functionName, e.err)
 }
 
+type inputsTypeCheck func(value interface{}) bool
+
 // Cache for evalArray-evaluations
 var evalExps = make(map[string]*govaluate.EvaluableExpression)
 
 // Functions used by `evalArray`
 var functions = map[string]govaluate.ExpressionFunction{
-	"ABS":         ensureFloats(absFunction),
-	"abs":         ensureFloats(absFunction),
-	"sqrt":        ensureFloats(sqrtFunction),
-	"SQRT":        ensureFloats(sqrtFunction),
+	"ABS":         typeCheck(isFloat64,absFunction),
+	"abs":         typeCheck(isFloat64,absFunction),
+	"sqrt":        typeCheck(isFloat64,sqrtFunction),
+	"SQRT":        typeCheck(isFloat64,sqrtFunction),
 	"ACCESS":      accessFunction,
 	"access":      accessFunction,
 	"ARRAY":       arrayFunction,
 	"array":       arrayFunction,
-	"AVG":         ensureFloats(averageFunction),
-	"avg":         ensureFloats(averageFunction),
-	"stdevp":      ensureFloats(stdevFunction),
-	"STDEVP":      ensureFloats(stdevFunction),
+	"AVG":         typeCheck(isFloat64,averageFunction),
+	"avg":         typeCheck(isFloat64,averageFunction),
+	"stdevp":      typeCheck(isFloat64,stdevFunction),
+	"STDEVP":      typeCheck(isFloat64,stdevFunction),
 	"IF":          ifFunction,
 	"If":          ifFunction,
 	"if":          ifFunction,
-	"MAX":         ensureFloats(maxFunction),
-	"Max":         ensureFloats(maxFunction),
-	"max":         ensureFloats(maxFunction),
-	"MIN":         ensureFloats(minFunction),
-	"Min":         ensureFloats(minFunction),
-	"min":         ensureFloats(minFunction),
-	"RANDBETWEEN": ensureFloats(randbetweenFunction),
-	"randbetween": ensureFloats(randbetweenFunction),
-	"ROUND":       ensureFloats(roundFunction),
-	"round":       ensureFloats(roundFunction),
-	"FLOOR":       ensureFloats(floorFunction),
-	"floor":       ensureFloats(floorFunction),
-	"CEILING":     ensureFloats(ceilingFunction),
-	"ceiling":     ensureFloats(ceilingFunction),
-	"trunc":       ensureFloats(truncFunction),
-	"TRUNC":       ensureFloats(truncFunction),
-	"SAFE_DIV":    ensureFloats(safeDivFuntion),
-	"Safe_div":    ensureFloats(safeDivFuntion),
-	"safe_div":    ensureFloats(safeDivFuntion),
-	"SCORE_TABLE": ensureFloats(scoreTableFunction),
-	"score_Table": ensureFloats(scoreTableFunction),
-	"score_table": ensureFloats(scoreTableFunction),
+	"MAX":         typeCheck(isFloat64,maxFunction),
+	"Max":         typeCheck(isFloat64,maxFunction),
+	"max":         typeCheck(isFloat64,maxFunction),
+	"MIN":         typeCheck(isFloat64,minFunction),
+	"Min":         typeCheck(isFloat64,minFunction),
+	"min":         typeCheck(isFloat64,minFunction),
+	"RANDBETWEEN": typeCheck(isFloat64,randbetweenFunction),
+	"randbetween": typeCheck(isFloat64,randbetweenFunction),
+	"ROUND":       typeCheck(isFloat64,roundFunction),
+	"round":       typeCheck(isFloat64,roundFunction),
+	"FLOOR":       typeCheck(isFloat64,floorFunction),
+	"floor":       typeCheck(isFloat64,floorFunction),
+	"CEILING":     typeCheck(isFloat64,ceilingFunction),
+	"ceiling":     typeCheck(isFloat64,ceilingFunction),
+	"trunc":       typeCheck(isFloat64,truncFunction),
+	"TRUNC":       typeCheck(isFloat64,truncFunction),
+	"SAFE_DIV":    typeCheck(isFloat64,safeDivFuntion),
+	"Safe_div":    typeCheck(isFloat64,safeDivFuntion),
+	"safe_div":    typeCheck(isFloat64,safeDivFuntion),
+	"SCORE_TABLE": typeCheck(isFloat64,scoreTableFunction),
+	"score_Table": typeCheck(isFloat64,scoreTableFunction),
+	"score_table": typeCheck(isFloat64,scoreTableFunction),
 	"strlen":      strlen,
 	"STRLEN":      strlen,
-	"SUM":         ensureFloats(sumFunction),
-	"Sum":         ensureFloats(sumFunction),
-	"sum":         ensureFloats(sumFunction),
+	"SUM":         typeCheck(isFloat64,sumFunction),
+	"Sum":         typeCheck(isFloat64,sumFunction),
+	"sum":         typeCheck(isFloat64,sumFunction),
 }
 
 func randbetweenFunction(args ...interface{}) (interface{}, error) {
@@ -352,14 +354,22 @@ func strlen(args ...interface{}) (interface{}, error) {
 	return (float64)(length), nil
 }
 
-func checkFloats(f func(args ...interface{}) (interface{}, error)) func(args ...interface{}) (interface{},error) {
+func isFloat64(value interface{}) bool {
+	switch value.(type) {
+	case float64:
+		return true
+	}
+	return false
+}
+
+func typeCheck(check inputsTypeCheck, f func(args ...interface{}) (interface{}, error)) func(args ...interface{}) (interface{},error) {
 	return func(args ...interface{}) (interface{}, error) {
 		for _, a := range args {
-			if v, ok := a.(float64); ok {
+			if check(a) {
 			} else {
 				return nil, &customFunctionError{
 					functionName: "sumFunction",
-					err:          fmt.Sprintf("Unsupported type to sum: expected '%v' to be a %T", a, v),
+					err:          fmt.Sprintf("Unsupported type to sum: expected '%v'", a),
 				}
 			}
 		}
